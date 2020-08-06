dcdbde0b4930985c14165808c4afbc9e2cc06637
A601-12 <mkii.dat@gmail.com>
2020-08-06 09:31:27 +0900

    完成

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,32 @@ namespace v0806
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.Location.X < 0)
+            {
+                vx = 10;
+            }else if(label1.Location.X > 580)
+            {
+                vx = -10;
+            }
+            if (label1.Location.Y < 0)
+            {
+                vy = 10;
+            }
+            else if (label1.Location.Y > 350)
+            {
+                vy = -10;
+            }
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
